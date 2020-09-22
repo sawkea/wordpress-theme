a35305fccd70dbd75cf6756b7491288bee2fd913
sawkea <shoareau@orange.fr>
2020-09-22 11:17:24 +0200

    Fonctions de bootstrap ok

--- wordpress/wp-content/themes/world-trip/functions.php
+++ wordpress/wp-content/themes/world-trip/functions.php
@@ -11,12 +11,23 @@ function register_menu(){
     register_nav_menu( 'header-menu', __( 'header-menu'));
 
 }
-// Ajout style Bootstrap via une fonction
+
+// Ajout style BOOTSTRAP via une fonction
 function montheme_register_assets () {
+    
     // enregistrer le style
-    wp_register_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
-    // utiliser le style
+    wp_register_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', []);
+    // enregistrer script js 
+    wp_register_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
+    wp_register_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', [], false, true);
+
+    //ne pas enregistrer je jquery de wordpress a faire attention pour des plugins wordpress
+    wp_deregister_script('jquery');
+
+    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', [], false, true);
+    // utiliser le style et les scripts
     wp_enqueue_style( 'bootstrap' );
+    wp_enqueue_script( 'bootstrap' );
 }
 
 // ACTIONS ___________________________________________________________________________________________________
