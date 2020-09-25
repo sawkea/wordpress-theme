9903e372224a17489e641cca9e502035b5a3cb1f
sawkea <shoareau@orange.fr>
2020-09-25 11:36:06 +0200

    texte h1 header et mobile ok

--- wordpress/wp-content/themes/world-trip/front-page.php
+++ wordpress/wp-content/themes/world-trip/front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<!-- Pour vrsion desktop -->
+<!-- Pour version desktop -->
 <!-- <h1><div class="mx-auto"><img class="img-fluid mt-5 mb-5" src="<?php echo get_template_directory_uri(); ?>/img/photo-hero.png" alt="Image hero"></div></h1>
 <p id="text-header">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, aliquam aut. Explicabo nam a eum quaerat quod voluptatibus, sapiente corrupti dolores velit amet hic iure facilis numquam similique impedit possimus!</p>
     
@@ -11,11 +11,11 @@
     
 <div id="about" class="mt-10"><?php the_content(); ?></div> -->
 
-<h1 class="mt-6 text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit, uibusdam, aliquam aut. Explicabo nam a eum quaerat quod voluptatibus, sapiente corrupti amet hic iure facilis similique impedit possimus !</h1>
+<h1 id="text-header" class="text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit, uibusdam, aliquam aut. Explicabo nam a eum quaerat quod voluptatibus, sapiente corrupti amet hic iure facilis similique impedit possimus !</h1>
 
 <div class="mx-auto"><img class="img-fluid mt-5 mb-5" src="<?php echo get_template_directory_uri(); ?>/img/photo-hero.png" alt="Image hero"></div>
     
-<!-- <div id="about" class="mt-10"><?php the_content(); ?></div> -->
+<div id="about" class="mt-10"><?php the_content(); ?></div> 
 
 
 <?php get_footer(); ?>
