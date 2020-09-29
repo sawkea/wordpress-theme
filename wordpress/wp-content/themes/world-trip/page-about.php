5a43e28addf58755a4c9249c10b3bc96b8db73f2
sawkea <shoareau@orange.fr>
2020-09-29 10:10:27 +0200

    Partie about mobile ok

--- wordpress/wp-content/themes/world-trip/page-about.php
+++ wordpress/wp-content/themes/world-trip/page-about.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
 
     <?php get_template_part('parts/about', 'page') ?>
-      
+    
 <?php get_footer(); ?>
--- wordpress/wp-content/themes/world-trip/parts/about.php
+++ wordpress/wp-content/themes/world-trip/parts/about.php
@@ -1,5 +1,5 @@
 <!-- Patie About présente sur la page d'accueil -->
-
+</div>
 <div id="about" class="mt-10"><img class="img-fluid mt-5" src="<?php echo get_template_directory_uri(); ?>/img/photo-about.png" alt="Image about"></div>
     <div class="container mb-5">
         <div class="about-relative">
@@ -11,3 +11,4 @@
                 <p class="text-justify">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, look even slightly believable. But the majority have suffered alteration in some ! There are many variations of passages of Lorem Ipsum available, or randomised words which don't look even slightly believable.</p> 
         </div>
     </div> 
+
